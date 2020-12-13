fe00287ac552806fd302af21aea1bbcbf0b47773
Nam Hoài Vũ <vunam722000@gmail.com>
2020-12-13 13:14:43 +0700

    thêm hàm repair

--- ChatterBot/App.py
+++ ChatterBot/App.py
@@ -19,7 +19,9 @@ my_bot = ChatBot("MyChatterBot", storage_adapter='chatterbot.storage.SQLStorageA
 trainer = ChatterBotCorpusTrainer(my_bot)
 
 my_bot.storage.drop()
-trainer.train("accessories")
+# trainer.train("accessories")
+trainer.train("G:\Desktop\Document20201\AI\ChatterBot17\ChatterBot\pcshop.yml")
+
 with open('data/conversation.json', 'w', encoding='utf-8') as file:
     file.flush()
     file.write('{}')
@@ -34,6 +36,7 @@ def json_conversation_save(userText, output):
         old_data.update(data)
         json.dump(old_data, file, ensure_ascii=False, indent=2)
         file.close()
+
 
 
 def accessories_link(type, model):
@@ -112,6 +115,98 @@ def get_accessories_response():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/get/repair")
+def get_repair_response():
+    phuKien = None
+
+    ## Đọc file json
+    file = open('G://Desktop//Document20201//AI//ChatterBot17//ChatterBot//data//repair.json')
+    data = json.load(file)
+        
+
+    timeOut = 0
+    msgAfterWait = ''
+    output = None
+
+    userText = request.args.get('msg')
+    userText = str.lower(userText)
+
+    if ('hỏng' in userText or 'không nghe được' in userText) and ('âm thanh' in userText or 'tiếng' in userText) or ('lỗi loa' in userText):
+        output = 'loi_loa'
+        phuKien = "thay_the_loa"
+    elif ('hỏng' in userText or 'không mở được' in userText or 'màn hình xanh' in userText) and ('ổ cứng' in userText or 'ssd' in userText or 'hdd' in userText):
+        output = 'loi_o_cung'
+        phuKien = "thay_the_o_cung"
+    elif 'sửa chữa' in userText:
+        output = 'sửa chữa'
+    elif 'thay thế' in userText:
+        if data["phu_kien"] == "thay_the_o_cung":
+            output = 'thay_the_o_cung'
+        else:
+            output = 'thay_the'
+    elif data["phu_kien"] == "thay_the_o_cung":
+        if 'ssd' in userText:
+            output = 'thay_the_ssd'
+            phuKien = 'thay_the_ssd'
+        elif 'hdd' in userText and data["phu_kien"]:
+            output = 'thay_the_hdd'
+            phuKien = 'thay_the_hdd'
+    elif data["phu_kien"] == "thay_the_ssd":
+        if '128' in userText:
+            phuKien = 'ssd_128'
+            output = 'thay_the'
+        elif '256' in userText:
+            phuKien = 'ssd_256'
+            output = 'thay_the'
+        else:
+            output = 'not_have'
+    elif data["phu_kien"] == "thay_the_hdd":
+        if '500' in userText:
+            phuKien = 'hdd_500'
+            output = 'thay_the'
+        elif '1' in userText and 'tb' in userText:
+            phuKien = 'hdd_1000'
+            output = 'thay_the'
+        else:
+            output = 'not_have'
+    else:
+        if 'hỏng' in userText or 'vấn đề' in userText:
+            output = 'vấn đề'
+
+    if phuKien:
+        data.update({ "phu_kien": phuKien })
+        with open('G://Desktop//Document20201//AI//ChatterBot17//ChatterBot//data//repair.json','w',encoding='utf-8') as user_dumped :
+            json.dump(data, user_dumped, ensure_ascii=False)
+
+    print(data["phu_kien"])
+    print(data[data["phu_kien"]])
+    
+    if output:
+        if output == 'thay_the':
+            output = str(my_bot.get_response(str(output)))
+            for i in data[data["phu_kien"]]["link"]:
+                output = output + "</br>" + i
+        else:
+            output = str(my_bot.get_response(str(output)))
+        
+        return {'output': output, 'timeOut': {'msg': msgAfterWait, 'milisecond': timeOut}}
+    else:
+        return None
+    
+
+@app.route("/get/general")
+def get_general_response():
+    return None
+    
+
+@app.route("/get/advisory")
+def get_advisory_response():
+    return None
+    
+@app.route("/get/guarantee")
+def get_guarantee_response():
+    return None
 
 if __name__ == "__main__":
     app.run()
