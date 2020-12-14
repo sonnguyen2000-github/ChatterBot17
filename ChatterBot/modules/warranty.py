7c0f22dffb6444c94d519f5a41ab62f6c7a06e11
Your Name <you@example.com>
2020-12-14 18:55:23 +0700

    toan

--- ChatterBot/modules/warranty.py
+++ ChatterBot/modules/warranty.py
@@ -46,6 +46,66 @@ def check_in(output, str):
             return True
     return False
 
+def getData_InforLaptop():
+    with open('data/bill.json','r', encoding="utf8") as bill_file:   
+        reader_loader = json.load(bill_file)
+    size = len(reader_loader["value_bill"])
+    listSerial = []
+    listPrice = []
+    listName = []
+    listDateBuy = []
+    listNameCustomer = []
+    listAll = []
+    for i in range(0, size):
+        listSerial.append(reader_loader["value_bill"][i]["serial"])
+        listPrice.append(reader_loader["value_bill"][i]["price"])
+        listName.append(reader_loader["value_bill"][i]["nameLaptop"])
+        listDateBuy.append(reader_loader["value_bill"][i]["dateBuy"])
+        listNameCustomer.append(reader_loader["value_bill"][i]["nameCustomer"])
+
+    listAll.append(listSerial)
+    listAll.append(listPrice)
+    listAll.append(listName)
+    listAll.append(listDateBuy)
+    listAll.append(listNameCustomer)
+
+    return listAll
+
+def check_Warranty_Serial(userText):
+    listAll = getData_InforLaptop()
+    listSerial = listAll[0]
+    # listPrice = listAll[1]
+    # listName = listAll[2]
+    # listDateBuy = listAll[3]
+    # listNameCustomer = listAll[4]
+
+    size = len(listSerial)
+    for i in range(0, size):
+        if (listSerial[i] in userText):
+            return True
+            break
+    return False
+
+def get_InforLaptop_serial(userText):
+    listAll = getData_InforLaptop()
+    listSerial = listAll[0]
+    listPrice = listAll[1]
+    listName = listAll[2]
+    listDateBuy = listAll[3]
+    listNameCustomer = listAll[4]
+    listInforLap = []
+
+    size = len(listSerial)
+    for i in range(0, size):
+        if (listSerial[i] in userText):
+            listInforLap.append(listSerial[i])
+            listInforLap.append(listPrice[i])
+            listInforLap.append(listName[i])
+            listInforLap.append(listDateBuy[i])
+            listInforLap.append(listNameCustomer[i])
+            return listInforLap
+            break
+    return "ko cos gi ca"
 
 def get_warranty_response(my_bot, request):
     userText = request.args.get('msg')
@@ -68,6 +128,14 @@ def get_warranty_response(my_bot, request):
             output = 'thông tin bảo hành sản phẩm'
         elif 'bảo hành theo' in userText:
             output = 'bảo hành theo số serial'
+    elif check_Warranty_Serial(userText) == True:
+        listInfor = []
+        listInfor = get_InforLaptop_serial(userText)
+        respon = "sản phẩm " + listInfor[2] + "(mã sản phẩm: " + listInfor[0] + ") " + "của khách hàng " + listInfor[4] + " được mua vào ngày " + listInfor[3]
+        if (check_warranty_Date(listInfor[3])):
+            return respon + "vẫn còn thời hạn bảo hành ạ"
+        else:
+            return respon + "đã hết hạn bảo hành ạ"
     elif ('laptop' in userText and 'bị hỏng' in userText) or ('laptop bị hỏng' in userText) or ('liệt phím' in userText) \
             or ('màn hình' in userText) or ('đơ' in userText) or ('lag' in userText) or ('chậm' in userText) or (
             'màn hinh bị' in userText):
@@ -77,10 +145,11 @@ def get_warranty_response(my_bot, request):
             output = 'laptop vẫn bị hỏng dù đã sửa chữa'
     elif 'mình yêu nhau' in userText:
         output = 'mình yêu nhau đi'
-    elif check_warranty(userText) == True:
-        output = 'vẫn còn thời hạn bảo hành'
-    elif check_warranty(userText) == False:
-        output = 'hết thời hạn bảo hành'
+    elif 'mua' in userText and 'ngày' in userText:
+        elif check_warranty_Date(userText) == True:
+            output = 'vẫn còn thời hạn bảo hành'
+        else:
+            output = 'hết thời hạn bảo hành'
     else:
         output = 'unknown'
 
